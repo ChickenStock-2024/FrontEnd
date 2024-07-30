f2e131f86e8d9b3ce4415f20610515cae4b363a0
soralee <pineapple223663@gmail.com>
2024-07-31 00:51:34 +0900

    Feat: 매수 창 구현

--- src/components/Trading/OrderSection/OrderStockBuy.jsx
+++ src/components/Trading/OrderSection/OrderStockBuy.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import QuantityButton from "./QuantityButton";
 
 const StockBuy = () => {
-  return <div>매수</div>;
+  return (
+    <div className="m-4">
+      <div className="mb-4">
+        <label htmlFor="price" className="font-bold">
+          가격
+        </label>
+        <div className="flex justify-between p-2 border border-gray-400 rounded-md">
+          <input
+            id="price"
+            type="number"
+            className="w-full focus:outline-none text-right"
+          />
+          <span className="text-gray-400">원</span>
+        </div>
+      </div>
+
+      <label htmlFor="quantity" className="font-bold">
+        수량
+      </label>
+      <div className="flex justify-between p-2 border border-gray-400 rounded-md">
+        <input
+          id="quantity"
+          type="number"
+          className="w-full focus:outline-none text-right"
+        />
+        <span className="text-gray-400">주</span>
+      </div>
+      <div>
+        <QuantityButton />
+      </div>
+    </div>
+  );
 };
 
 export default StockBuy;
---
+++ src/components/Trading/OrderSection/QuantityButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuantityButton = ({ percent }) => {
+  return <button>%</button>;
+};
+
+export default QuantityButton;
