bb6a7d3a13b18cba10dd73cb854bcbb1dcacebc3
Sooyeon Back <sooyeon.back12@gmail.com>
2024-08-13 10:43:37 +0900

    Feat: OAuth kakao 로그인

---
+++ src/components/Login/KakaoLogin.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { defaultInstance } from "../../api/axios";
+
+import useLoginUserInfoStore from "../../store/useLoginUserInfoStore";
+import useCompetitionInfoStore from "../../store/useCompetitionInfoStore";
+import useNotificationInfoStore from "../../store/useNotificationInfoStore";
+
+// window.open(
+//   "https://chickenstock.givendragon.site/api/auth/login/kakao",
+//   "newWindow"
+// );
+
+const kakaoLogin = () => {
+  const nav = useNavigate();
+  useEffect(() => {
+    getKakaoLogin();
+  }, []);
+
+  // 4. 카카오 로그인 axios
+  const getKakaoLogin = async () => {
+    try {
+      console.log("카카오 로그인 전");
+
+      // Axios의 응답 객체에서 직접 checkedEmail 추출
+      const response = await defaultInstance.get("/auth/login/kakao");
+
+      alert("카카오 로그인 완료~!!");
+      console.log("카카오 로그인 response: ", response.data);
+
+      // // 진행 상태 업데이트
+
+      nav("/");
+    } catch (error) {
+      console.log(error);
+      alert(
+        "카카오 로그인 중 오류: " +
+          (error.response ? error.response.data.message : error.message)
+      );
+    }
+  };
+};
+
+export default kakaoLogin;
--- src/store/useLoginUserInfoStore.jsx
+++ src/store/useLoginUserInfoStore.jsx
@@ -37,7 +37,7 @@ const useLoginUserInfoStore = create(
     }),
     {
       name: "loginUserInfo", // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => sessionStorage), // (optional)이기 때문에 해당 줄을 적지 않으면 'localStorage'가 기본 저장소로 사용된다.
+      // storage: createJSONStorage(() => sessionStorage), // (optional)이기 때문에 해당 줄을 적지 않으면 'localStorage'가 기본 저장소로 사용된다.
     }
   )
 );
