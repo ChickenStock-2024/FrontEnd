05087206c3eb50a2362eba2553b5e0a798e4a629
soralee <pineapple223663@gmail.com>
2024-08-14 15:36:07 +0900

    Feat: 주문 화면에서 내잔고 출력

--- src/components/Trading/OrderSection/OrderMain/OrderStockInput.jsx
+++ src/components/Trading/OrderSection/OrderMain/OrderStockInput.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
 import QuantityButton from "./QuantityButton";
 import PriceToggle from "./PriceToggle";
+import useLoginUserinfoStore from "./../../../../store/useLoginUserInfoStore";
 
 const OrderStockInput = ({
   activeTabOption,
@@ -15,7 +16,9 @@ const OrderStockInput = ({
   setIsMarketPrice,
 }) => {
   // 임의의 초기 잔고
-  const myAccount = 1000000;
+  const myAccount = useLoginUserinfoStore(
+    (state) => state.loginUserInfo.balance || 1000000
+  );
 
   // 임의의 해당 종목 보유량
   const myQuantity = 200;
