6bf4089a5e0ac6a9465aa1a71b46d4ed67a69c0a
Sooyeon Back <sooyeon.back12@gmail.com>
2024-08-14 21:55:00 +0900

    Feat: ProfilePage UserSearch

--- src/components/Profile/ProfileUserSearchTab/ProfileUserSearchTab.jsx
+++ src/components/Profile/ProfileUserSearchTab/ProfileUserSearchTab.jsx
@@ -3,11 +3,26 @@ import { useState, useEffect } from "react";
 import ProfileUserSearchTabSearch from "./ProfileUserSearchTabSearch.jsx";
 // import ProfileUserSearchTabResult from "./ProfileUserSearchTabResult.jsx";
 import ProfileUserSearchTabAxios from "./ProfileUserSearchTabAxios.js";
+import { defaultInstance } from "../../../api/axios.jsx";
 // import Input from "../../Input.jsx";
 
 const ProfileUserSearchTab = () => {
   const [users, setUsers] = useState(null);
   const [search, setSearch] = useState("");
+
+  const getSearchUsers = async () => {
+    try {
+      const response = await defaultInstance.get(
+        `/user/search?value=${search}`
+      );
+    } catch (error) {
+      console.log(error);
+      alert(
+        "유저 검색을 실패했습니다: " +
+          (error.response ? error.response.data.message : error.message)
+      );
+    }
+  };
 
   useEffect(() => {
     if (search === "") {
